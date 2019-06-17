a48b40708940bde12423aaed3ff9e43848f0d3c6
Andre Cloete <andre@yoco.co.za>
2019-06-17 22:40:29 +0200

    feat: Adds process_song_data

--- Project 4 - Data Lakes with Spark/etl.py
+++ Project 4 - Data Lakes with Spark/etl.py
@@ -2,8 +2,7 @@ import configparser
 from datetime import datetime
 import os
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf, col
-from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
+from pyspark.sql import functions as F
 
 config = configparser.ConfigParser()
 config.read('dl.cfg')
@@ -60,10 +59,9 @@ def create_spark_session():
     """
     Create and return a spark session
     """
-    spark = SparkSession \
-        .builder \
-        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
-        .getOrCreate()
+    spark = SparkSession.builder.getOrCreate()
+#         .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
+        
     return spark
 
 
@@ -76,21 +74,26 @@ def process_song_data(spark, input_data, output_data):
     """
     # get filepath to song data file
     song_data = input_data+'song_data/*/*/*/*.json'
-    
+
     # read song data file
     df = spark.read.json(song_data)
 
     # extract columns to create songs table
-    songs_table = ''
-    
+    songs_table = df.select(['song_id','title','artist_id','year','duration'])
+
     # write songs table to parquet files partitioned by year and artist
-    songs_table = ''
+    songs_table.write.parquet(output_data+'/songs/'+'songs_table.parquet',
+                              partitionBy=['year','artist_id'])
 
     # extract columns to create artists table
-    artists_table = '' 
-    
+    artists_table = df.select(['artist_id','artist_name','artist_location','artist_latitude','artist_longitude']) \
+                    .withColumnRenamed('artist_name','artist') \
+                    .withColumnRenamed('artist_location','location') \
+                    .withColumnRenamed('artist_longitude','longitude') \
+                    .withColumnRenamed('artist_latitude','latitude')
+
     # write artists table to parquet files
-    artists_table = ''
+    artists_table.write.parquet(output_data+'/artists/'+'artists_table.parquet')
 
 
 def process_log_data(spark, input_data, output_data):
