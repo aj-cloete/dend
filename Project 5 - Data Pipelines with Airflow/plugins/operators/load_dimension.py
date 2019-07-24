8affe37466bc10b1bc0dbe4d09b707fab2a59c5d
André <ajcloete@gmail.com>
2019-07-24 22:40:59 +0200

    feat: Add load_dimention.py functionality

--- Project 5 - Data Pipelines with Airflow/plugins/operators/load_dimension.py
+++ Project 5 - Data Pipelines with Airflow/plugins/operators/load_dimension.py
@@ -3,20 +3,42 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class LoadDimensionOperator(BaseOperator):
-
+    '''
+    sql_statement (string): [required] The SQL statement to run on the target database
+    target_db_conn_id (string): [required] Airflow conn_id to the database on which the sql_statement should run
+    target_table (string): [required] The name of the table into which the results from sql_statement should be loaded
+    loading_type (string): The 'delete-load' (default) option will drop-and-swap load the data.  The 'append' option will append new data to the existing data
+    '''
+    
     ui_color = '#80BD9E'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 sql_statement,
+                 target_db_conn_id,
+                 target_table,
+                 loading_type='delete-load'
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.sql_statement = sql_statement
+        self.target_db_conn_id = target_db_conn_id
+        assert len(target_table) > 0, "The target_table name cannot be an empty string"
+        self.target_table = target_table
+        assert loading_type in ('delete-load','append'), "loading_type must be one of ('delete-load','append')"
+        self.loading_type = loading_type
+        self.log.info(f'Loading type: {self.loading_type}')
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        hook = PostgresHook(self.target_db_conn_id)
+        if self.loading_type=='delete':
+            self.log.info(f"Clearing the data from {self.target_table}")
+            truncate_statement = f"TRUNCATE {self.target_table}"
+            hook.run(truncate_statement)
+        
+        self.log.info(f"Loading the fact table into the database at connection '{self.target_db_conn_id}'")
+        insert_statement = f"INSERT INTO {self.target_table} \n{self.sql_statement}"
+        self.log.info(f"Running sql: \n{insert_statement}")
+        hook.run(insert_statement)
+        self.log.info(f"Successfully completed insert into {self.target_table}")
+
