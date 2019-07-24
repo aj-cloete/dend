29ca3d8b081a79851d3aaec3e6a01a9064c80dfe
André <ajcloete@gmail.com>
2019-07-24 22:39:05 +0200

    feat: Adds data_quality.py functionality

--- Project 5 - Data Pipelines with Airflow/plugins/operators/data_quality.py
+++ Project 5 - Data Pipelines with Airflow/plugins/operators/data_quality.py
@@ -3,20 +3,39 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class DataQualityOperator(BaseOperator):
+    '''
+    Run tests using the provided sql_test and compare the results against the provided expected_result. 
+    Raise an error if the result is not equal to the expected result
+    conn_id (string): [required] Airflow conn_id to the database on which the tests should be run
+    sql_test (string or list of strings): [required] SQL statement(s) to run as tests
+    expected_result (string or list of strings): [required] Expected result(s) against which to compare the sql_test result(s)
+    '''
 
     ui_color = '#89DA59'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 conn_id,
+                 sql_test,
+                 expected_result,
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
-
+        self.conn_id = conn_id
+        if isinstance(sql_test,str):
+            self.sql_test = [sql_test]
+            self.expected_result = [expected_result]
+        else:
+            assert len(sql_test)==len(expected_result), "Must provide the same number of tests and results"
+            self.sql_test = sql_test
+            self.expected_result = expected_result
+        
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        self.log.info(f'Running Data Quality {'tests' if len(self.sql_test)>1 else 'test'}')
+        hook = PostgresHook(self.conn_id)
+        for (test_sql, expectation) in zip(self.sql_test,self.expected_result):
+            result = hook.get_first(test_sql)[0]
+            if str(result)!=expectation:
+                self.log.info(f"Running test SQL: \n{test_sql}")
+                raise ValueError(f'This test did not pass \n{test_sql}')
+        self.log.info("All tests passed!")
