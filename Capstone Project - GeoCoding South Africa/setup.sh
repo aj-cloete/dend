db477458b9066d45aa549dd9b99a0d59dbaa5009
André <ajcloete@gmail.com>
2019-08-18 03:26:09 +0200

    feat: Add support for spatial join in geopandas

--- Capstone Project - GeoCoding South Africa/setup.sh
+++ Capstone Project - GeoCoding South Africa/setup.sh
@@ -16,6 +16,7 @@ if [ ! -x /usr/local/bin/python3.7 ]; then
             sudo apt-get install build-essential checkinstall -y
             sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
                 libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev -y
+            sudo apt install libspatialindex-dev -y
             cd /usr/src
             sudo wget https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tgz
             sudo tar xzf Python-3.7.4.tgz
@@ -27,6 +28,7 @@ if [ ! -x /usr/local/bin/python3.7 ]; then
             cd /usr/src
             sudo wget https://www.python.org/ftp/python/3.7.4/python-3.7.4-macosx10.9.pkg
             sudo installer -pkg python-3.7.4-macosx10.9.pkg -target /
+            brew install spatialindex
         ;;
         *)
             echo "${_UNAME_OUT} is unsupported."
@@ -40,8 +42,10 @@ cd $BASEDIR
 source "${BASEDIR}/capstone/bin/activate"
 
 # Install the required packages
-pip install -r requirements.txt --upgrade
+pip install -r requirements.txt
 
 /bin/bash "${BASEDIR}/get_data.sh"
 
-echo remember to activate the environment using: source capstone/bin/activate
+echo remember to activate the environment using: source capstone/bin/activate before running any of the other scrips
+
+    
